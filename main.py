1bce07a58fc5fc275b18d11fc43c41e44e97f448
Eduardo <eduado.bernad@northius.com>
2023-11-20 18:46:40 +0100

    Add logs and code reformat

--- main.py
+++ main.py
@@ -3,110 +3,86 @@ import os
 import random
 import schedule
 import time
+import logging
 from playwright.sync_api import sync_playwright
 from datetime import datetime, timedelta
 
+# Configura el registro para almacenar los mensajes en un archivo
+logging.basicConfig(filename='output.log', level=logging.INFO, format='%(asctime)s - %(message)s')
 
-def create_random_time(current_time):
-    # Convertir la cadena de texto a un objeto datetime
-    time_obj = datetime.strptime(current_time, "%H:%M")
+class TimeManager:
+    def __init__(self, entry_time="08:00", exit_time="18:00", friday_exit_time="15:00"):
+        # Crea un tiempo aleatorio para la entrada, salida y salida de los viernes
+        self.entry_time = self.create_random_time(entry_time)
+        self.exit_time = self.create_random_time(exit_time)
+        self.friday_exit_time = self.create_random_time(friday_exit_time)
 
-    # Generar un número aleatorio entre 0 y 15
-    random_minutes = random.randint(0, 15)
+    def create_random_time(self, current_time):
+        # Convierte la hora actual a un objeto datetime y luego añade o resta minutos
+        time_obj = datetime.strptime(current_time, "%H:%M")
+        random_minutes = random.randint(0,15)
+        new_time = time_obj + timedelta(minutes=random_minutes * random.choice([-1,1]))
+        return new_time.strftime("%H:%M")
 
-    # Elegir aleatoriamente entre sumar o restar el número de minutos generados
-    operation = random.choice([-1, 1])
+class Bot:
+    def __init__(self, time_manager, url, username, password):
+        self.time_manager = time_manager
+        self.url = url
+        self.username = username
+        self.password = password
 
-    # Calcular el nuevo tiempo sumando o restando los minutos aleatorios
-    new_time = time_obj + timedelta(minutes=random_minutes * operation)
+    def login_and_click_button(self):
+        # Inicia el navegador, navega al sitio web, llena los campos y hace click en el botón
+        with sync_playwright() as playwright:
+            browser = playwright.chromium.launch()
+            context = browser.new_context()
+            page = context.new_page()
+            page.goto(self.url)
+            page.fill('input[name="username"]', self.username)
+            page.fill('input[name="password"]', self.password)
+            page.click('button[type="submit"]')
+            page.wait_for_load_state("networkidle")
+            page.click('button[id="btn-check"]')
+            browser.close()
 
-    # Convertir el nuevo tiempo a formato de cadena de texto
-    new_time_str = new_time.strftime("%H:%M")
+    def run_script(self):
+        # Revisa la hora actual y si es el tiempo de entrada o salida, realiza el login
+        current_time = time.strftime("%H:%M")
+        print("entra")
+        logging.info(f"Se ha fichado la Entrada {self.time_manager.entry_time}")
+        if (time.strftime("%A") in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+            and current_time == self.time_manager.entry_time):
+            self.login_and_click_button()
+            logging.info(f"Se ha fichado la Entrada {self.time_manager.entry_time}")
+            self.time_manager.entry_time = self.time_manager.create_random_time("08:00")
 
-    return new_time_str
+        if (time.strftime("%A") in ["Monday", "Tuesday", "Wednesday", "Thursday"]
+            and current_time == self.time_manager.exit_time):
+            self.login_and_click_button()
+            logging.info(f"Se ha fichado la Salida a las {self.time_manager.exit_time}")
+            self.time_manager.exit_time = self.time_manager.create_random_time("18:00")
 
-
-global HORA_ENTRADA 
-HORA_ENTRADA = create_random_time("08:00")
-global HORA_SALIDA 
-HORA_SALIDA = create_random_time("18:00")
-global HORA_SALIDA_VIERNES 
-HORA_SALIDA_VIERNES= create_random_time("15:00")
-
-
-def login_and_click_button(url, username, password):
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch()
-        context = browser.new_context()
-        page = context.new_page()
-
-        # Navegar a la página de inicio de sesión
-        page.goto(url)
-
-        # Rellenar el formulario de inicio de sesión
-        page.fill('input[name="username"]', username)
-        page.fill('input[name="password"]', password)
-
-        # Hacer clic en el botón de inicio de sesión
-        page.click('button[type="submit"]')
-
-        # Esperar a que se cargue la siguiente página
-        page.wait_for_load_state("networkidle")
-
-        # Realizar otras acciones después de iniciar sesión
-        page.click('button[id="btn-check"]')
-
-        # Cerrar el navegador
-        browser.close()
-
-
-def run_script():
-    # Obtener la hora actual
-    current_time = time.strftime("%H:%M")
-    global HORA_ENTRADA
-    global HORA_SALIDA
-    global HORA_SALIDA_VIERNES 
-
-    # Verificar si es de lunes a viernes y es la hora deseada
-    if (time.strftime("%A") in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"] and current_time == HORA_ENTRADA):
-        login_and_click_button(args.url, args.username, args.password)
-        HORA_ENTRADA = create_random_time("08:00")
-
-    # Verificar si es de lunes a jueves y es la hora deseada
-    if (time.strftime("%A") in ["Monday", "Tuesday", "Wednesday", "Thursday"] and current_time == HORA_SALIDA): 
-        HORA_SALIDA = create_random_time("18:00")
-        login_and_click_button(args.url, args.username, args.password)
-
-    # Verificar si es viernes y es la hora deseada
-    if time.strftime("%A") == "Friday" and current_time == HORA_SALIDA_VIERNES:
-        HORA_SALIDA_VIERNES = create_random_time("15:00")
-        login_and_click_button(args.url, args.username, args.password)
-
+        # Verificar si es viernes y es la hora deseada
+        if (time.strftime("%A") == "Friday" and current_time == self.time_manager.friday_exit_time):        
+            self.login_and_click_button()
+            logging.info(f"Se ha fichado la Salida del Viernes a las {self.time_manager.friday_exit_time}")
+            self.time_manager.exit_time = self.time_manager.create_random_time("15:00")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Realiza el inicio de sesión en una página web y pulsa un botón."
-    )
-    parser.add_argument(
-        "--url", default=os.getenv("URL"), help="URL de la página de inicio de sesión"
-    )
-    parser.add_argument(
-        "--username",
-        default=os.getenv("USERNAME"),
-        help="Nombre de usuario para iniciar sesión",
-    )
-    parser.add_argument(
-        "--password",
-        default=os.getenv("PASSWORD"),
-        help="Contraseña para iniciar sesión",
-    )
-
+    logging.info(f"Se ejecuta la App")
+    parser = argparse.ArgumentParser(description="Realiza el inicio de sesión en una página web y pulsa un botón.")
+    parser.add_argument("--url", default=os.getenv("URL"), help="URL de la página de inicio de sesión")
+    parser.add_argument("--username", default=os.getenv("USERNAME"), help="Nombre de usuario para iniciar sesión")
+    parser.add_argument("--password", default=os.getenv("PASSWORD"), help="Contraseña para iniciar sesión")
     args = parser.parse_args()
-    current_time = time.strftime("%H:%M")
-    # Comprueba cada 60 segundos si se cumple alguna condicion
-    schedule.every(60).seconds.do(run_script)
 
-    # Ejecutar el script en bucle
+    time_manager = TimeManager()  # Crea una instancia de TimeManager
+    bot = Bot(time_manager, args.url, args.username, args.password)  # Crea una instancia del Bot
+
+    # Programa el bot para comprobar cada 60 segundos si se cumple alguna condición
+    schedule.every(2).seconds.do(bot.run_script)
+
+    # Ejecuta el script en bucle
     while True:
         schedule.run_pending()
         time.sleep(1)
